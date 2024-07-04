30fce7d42fb170b7c7fd8bad2629c8668b51101a
tgodoy12 <112527761+tgodoy12@users.noreply.github.com>
2024-07-04 21:00:32 +0000

    all components added

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron bg-light pb-5 px-4">
+            <h1 className="display-4">Hello, world!</h1>
+            <p className="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi eaque architecto eos ex esse amet, 
+                corrupti eum necessitatibus quisquam consequatur commodi repudiandae quia, et dignissimos facilis enim rerum 
+                laborum quibusdam?
+            </p>
+
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+    );
+}
+
+export default Jumbotron;
